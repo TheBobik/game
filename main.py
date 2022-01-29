672340350116348138193e5e82665b1218f6d1fe
Makevich30 <84179265+Makevich30@users.noreply.github.com>
2022-01-29 10:51:08 +0400

    Закончили игру

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import pygame
-import pygame_widgets as pw
 import os
 import random
+import sys
 
 
-def load_image(name, color_key=None):
+def load_image(name, color_key=None, transform=None):
     fullname = os.path.join('data', name)
     try:
         image = pygame.image.load(fullname)
@@ -16,12 +16,22 @@ def load_image(name, color_key=None):
         if color_key == -1:
             color_key = image.get_at((0, 0))
         image.set_colorkey(color_key)
+    if transform:
+        image = pygame.transform.scale(image, (40, 40))
     return image
 
 
 pygame.init()
 FPS = 50
 size = 850
+oxy = 100
+player = None
+ready = False
+rocket_ready = False
+rocket_parts = 0
+scr = 0
+map_id = 0
+name_player = ''
 screen_size = (size, size)
 screen = pygame.display.set_mode(screen_size)
 
@@ -29,12 +39,13 @@ tile_images = {
     'rocket': load_image('broken_rocket.png'),
     'cave': load_image('cave.png'),
     'oxygen': load_image('O2.png'),
-    'oxygent': load_image('oxygent.png'),
     'empty1': load_image('moon1.png'),
     'empty2': load_image('moon2.png'),
     'empty3': load_image('invisible.png')
 }
 player_image = load_image('astronaut.png')
+peace_of_rocket = load_image('peace_of_rocket.png')
+o2 = load_image('draw_o2.png')
 bg = load_image('space.jpg')
 
 tile_width = tile_height = 50
@@ -79,12 +90,6 @@ class Camera:
         self.dy = 0
 
 
-oxy = 100
-player = None
-ready = False
-minigames = [1]
-map_id = 1
-rocket_parts = 0
 clock = pygame.time.Clock()
 sprite_group = pygame.sprite.Group()
 hero_group = pygame.sprite.Group()
@@ -119,56 +124,79 @@ def generate_level(level):
 
 
 def move(hero, movement):
-    global ready
+    global ready, rocket_ready
     if oxy > 0:
         x, y = hero.pos
         if movement == "up":
             if y > 0 and level_map[y - 1][x] != "`":
-                if level_map[y - 1][x] == "." or level_map[y - 1][x] == ",":
+                if level_map[y - 1][x] == "." or level_map[y - 1][x] == "," or level_map[y - 1][x] == "@":
                     oxygen('minus')
+                    score('step')
                 elif level_map[y - 1][x] == "0":
                     oxygen('full')
+                    score('fullo')
                     Tile('empty1', x, y - 1)
                 if level_map[y - 1][x] == "#":
                     ready = True
                 else:
                     ready = False
+                if level_map[y - 1][x] == "!":
+                    if rocket_parts == 1:
+                        rocket_ready = True
                 hero.move(x, y - 1)
+
         elif movement == "down":
             if y < max_y and level_map[y + 1][x] != "`":
-                if level_map[y + 1][x] == "." or level_map[y + 1][x] == ",":
+                if level_map[y + 1][x] == "." or level_map[y + 1][x] == "," or level_map[y + 1][x] == "@":
                     oxygen('minus')
+                    score('step')
                 elif level_map[y + 1][x] == "0":
                     oxygen('full')
+                    score('fullo')
                     Tile('empty1', x, y + 1)
                 if level_map[y + 1][x] == "#":
                     ready = True
                 else:
                     ready = False
+                if level_map[y + 1][x] == "!":
+                    if rocket_parts == 1:
+                        rocket_ready = True
                 hero.move(x, y + 1)
+
         elif movement == "left":
             if x > 0 and level_map[y][x - 1] != "`":
-                if level_map[y][x - 1] == "." or level_map[y][x - 1] == ",":
+                if level_map[y][x - 1] == "." or level_map[y][x - 1] == "," or level_map[y][x - 1] == "@":
                     oxygen('minus')
+                    score('step')
                 elif level_map[y][x - 1] == "0":
                     oxygen('full')
+                    score('fullo')
                     Tile('empty1', x - 1, y)
                 if level_map[y][x - 1] == "#":
                     ready = True
                 else:
                     ready = False
+                if level_map[y][x - 1] == "!":
+                    if rocket_parts == 1:
+                        rocket_ready = True
                 hero.move(x - 1, y)
+
         elif movement == "right":
             if x < max_x and level_map[y][x + 1] != "`":
-                if level_map[y][x + 1] == "." or level_map[y][x + 1] == ",":
+                if level_map[y][x + 1] == "." or level_map[y][x + 1] == "," or level_map[y][x + 1] == "@":
                     oxygen('minus')
+                    score('step')
                 elif level_map[y][x + 1] == "0":
                     oxygen('full')
+                    score('fullo')
                     Tile('empty1', x + 1, y)
                 if level_map[y][x + 1] == "#":
                     ready = True
                 else:
                     ready = False
+                if level_map[y][x + 1] == "!":
+                    if rocket_parts == 1:
+                        rocket_ready = True
                 hero.move(x + 1, y)
 
 
@@ -182,11 +210,84 @@ def oxygen(status):
     print(oxy)
 
 
-def message(msg, k):
-    x, y = screen_size
-    text_style = pygame.font.SysFont("bahnschrift", 25)
-    mesg = text_style.render(msg, True, (155, 0, 0))
-    screen.blit(mesg, (x // 2.5 - 5 ** k, y // 3 + 35 * k))
+def message(msg, k=1):
+    style = pygame.font.SysFont("malgungothic", 25)
+    mesg = style.render(msg, True, (155, 0, 0))
+    screen.blit(mesg, (size // 2.5 - 5 ** k, size // 3 + 35 * k))
+
+
+def score(status='draw'):
+    global scr
+
+    if status == 'step':
+        scr += 10
+    elif status == 'fullo':
+        scr += 100
+    elif status == 'rocketpart':
+        scr += 1000
+    if status == 'draw':
+        pygame.draw.rect(screen, (128, 128, 128), (605, 0, 245, 55))
+        pygame.draw.rect(screen, (255, 255, 255), (610, 5, 235, 45))
+        style = pygame.font.SysFont("aesthetic", 35)
+        text = style.render(f'Your score: {scr}', True, (128, 128, 128))
+        screen.blit(text, (615, 17))
+
+
+def start_screen():
+    global map_id
+    intro_text = 'Нажмите клавиши от 1 до 3 чтобы выбрать уровень!'
+
+    fon = pygame.transform.scale(load_image('zastavka'), screen_size)
+    screen.blit(fon, (0, 0))
+    style = pygame.font.SysFont("aesthetic", 35)
+    text = style.render(intro_text, True, (148, 0, 211))
+    screen.blit(text, (125, 340))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_1:
+                    map_id = 1
+                    return
+                elif event.key == pygame.K_2:
+                    map_id = 2
+                    return
+                elif event.key == pygame.K_3:
+                    map_id = 3
+                    return
+
+        pygame.display.flip()
+        clock.tick(FPS)
+
+
+def end_screen():
+
+    fon = pygame.transform.scale(load_image('end.png'), screen_size)
+    screen.blit(fon, (0, 0))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN:
+                terminate()
+
+        pygame.display.flip()
+        clock.tick(FPS)
+
+
+def score_consumables():
+    pygame.draw.rect(screen, (128, 128, 128), (0, 0, 155, 55))
+    pygame.draw.rect(screen, (255, 255, 255), (5, 5, 145, 45))
+    screen.blit(o2, (6, 10))
+    screen.blit(peace_of_rocket, (80, 11))
+    style = pygame.font.SysFont("aesthetic", 35)
+    text = style.render(f'{oxy // 10}', True, (128, 128, 128))
+    text2 = style.render(f'{rocket_parts}/1', True, (128, 128, 128))
+    screen.blit(text, (35, 17))
+    screen.blit(text2, (105, 17))
 
 
 def terminate():
@@ -194,6 +295,7 @@ def terminate():
     quit()
 
 
+start_screen()
 camera = Camera()
 level_map = load_level(f'map{map_id}.txt')
 hero, max_x, max_y = generate_level(level_map)
@@ -204,24 +306,7 @@ def gameloop():
     global oxy, rocket_parts
 
     while oxy == 0:
-        message('Вы проиграли!', 2)
-        message('Нажмите "Q" чтобы выйти из игры', 3)
-        pygame.display.flip()
-
-        for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_q:
-                    terminate()
-
-    while rocket_parts == map_id:
-        message('Вы проиграли!', 2)
-        message('Нажмите "Q" чтобы выйти из игры', 3)
-        pygame.display.flip()
-
-        for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_q:
-                    terminate()
+        os.system('python end.py')
 
     while oxy != 0:
         for event in pygame.event.get():
@@ -238,20 +323,37 @@ def gameloop():
                     move(hero, "right")
                 if ready:
                     if event.key == pygame.K_y:
-                        num_game = random.choice(minigames)
-                        os.system(f'python minigame{num_game}.py')
+                        '''вы действительнохотите начать игру?'''
+                        os.system('python minigame.py')
+                        score('rocketpart')
                         rocket_parts += 1
-                        minigames.remove(num_game)
+                        print(rocket_parts)
         screen.blit(bg, (0, 0))
         sprite_group.draw(screen)
         hero_group.draw(screen)
+        score()
+        score_consumables()
         clock.tick(FPS)
+
         if ready:
             message('Нажмите "Y" для начала мини игры', 1)
             pygame.display.flip()
+
+        if rocket_ready:
+            message('Нажмите "Y" чтобы попробывать восстановить ракету!', 1)
+            pygame.display.flip()
+
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_y:
+                        print(1)
+                        end_screen()
+
         pygame.display.flip()
+
         if oxy == 0:
             gameloop()
+
     terminate()
 
 
